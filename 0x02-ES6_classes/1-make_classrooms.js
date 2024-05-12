82671e15bd92a1bf6c63b5524034c53787f29277
Eze Onyedikachi <thekacchi1@gmail.com>
2024-05-12 14:33:04 +0000

    ling trsts

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,8 +1,9 @@
-import ClassRoom from './0-classroom.js';
+// 1-make_classrooms.js
+import ClassRoom from './0-classroom'; // Remove ".js" file extension
 
 function initializeRooms() {
   const roomSizes = [19, 20, 34];
-  const rooms = roomSizes.map(size => new ClassRoom(size));
+  const rooms = roomSizes.map((size) => new ClassRoom(size)); // Add parentheses around "size"
   return rooms;
 }
 
