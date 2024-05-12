1c9eb998fc710a806e7930bb2f4985530ff5748a
Eze Onyedikachi <thekacchi1@gmail.com>
2024-05-12 16:28:37 +0000

    fix airport

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,28 +1,13 @@
-// 7-airport .js
+// 7-airport.js
+class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
 
-export default class Airport{
-	constructor(name, code) {
-		this._name = name;
-		this._code = code;
-	}
-
-	get name() {
-		return this._name;
-	}
-
-	set name(value) {
-		this._name = value;
-	}
-
-	get code() {
-		return this._code;
-	}
-
-	set code(value) {
-		this._code = value;
-	}
-
-	toString() {
-		return `[object ${this._code}]`;
-	}
+  toString() {
+    return `object [${this._code}]`;
+  }
 }
+
+export default Airport;
