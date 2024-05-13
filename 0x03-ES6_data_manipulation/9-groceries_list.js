bbad707a63841cfcf6393a3c3d3610dbe848d89c
Eze Onyedikachi <thekacchi1@gmail.com>
2024-05-13 09:14:04 +0000

    tasks 8-11

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,12 @@
+// 9-groceries_list.js
+function groceriesList() {
+  const groceriesMap = new Map();
+  groceriesMap.set('Apples', 10);
+  groceriesMap.set('Tomatoes', 10);
+  groceriesMap.set('Pasta', 1);
+  groceriesMap.set('Rice', 1);
+  groceriesMap.set('Banana', 5);
+  return groceriesMap;
+}
+
+export default groceriesList;
