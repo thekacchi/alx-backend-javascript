8f1ff288ad1b92ec510360f3c5820de529512911
Eze Onyedikachi <thekacchi1@gmail.com>
2024-05-13 09:22:58 +0000

    check adjustments

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -5,6 +5,6 @@ export default function cleanSet(set, string) {
   }
   return [...set]
     .filter((str) => (str !== undefined ? str.startsWith(string) : ''))
-    .map((str) => ((tr !== undefined ? str.slice(string.length) : ''))
+    .map((str) => (str !== undefined ? str.slice(string.length) : ''))
     .join('-');
 }
