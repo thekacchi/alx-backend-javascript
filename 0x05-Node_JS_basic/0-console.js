ed9ca6047e2bfd0522839b215dcddc6e7302c1b4
Onyedikachi Eze <thekacchi1@gmail.com>
2024-06-17 19:33:20 +0000

    files ip

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Eze Onyedikachi  <https://github.com/thekacchi>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
